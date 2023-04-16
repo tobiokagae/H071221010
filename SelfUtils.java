370012ef03b9f9e8f6b618d363994e426be35ea5
tobiokagae <128783688+tobiokagae@users.noreply.github.com>
2023-04-17 00:20:04 +0800

    Delete SelfUtils.java

--- SelfUtils.java
+++
@@ -1,8 +0,0 @@
-public class SelfUtils {
-    public static void displaySelfData() {
-        String nama = "Andi Alisha Faiqihah";
-        String NIM = "H071221010";
-        System.out.println("Nama        : " + nama);
-        System.out.println("NIM         : " + NIM);
-    }
-}
